e7ff1d16e4776513e0d72536f5867afbc442280f
Ana Beatris <anabeatris050407@icloud.com>
2025-07-08 16:45:25 -0300

    Código aprimorado com graus de dificuldade, mensagens interativas e melhores informações das jogadas!

--- projetos-iniciais-python/jogos/Jogo_advinhação.py
+++ projetos-iniciais-python/jogos/Jogo_advinhação.py
@@ -1,37 +1,81 @@
 import random
 
-print('Este é um jogo de adivinhação!')
-print('Tente adivinhar um número de 1 a 100')
+print('🎮 Este é um jogo de adivinhação!')
+print('Tente adivinhar o número secreto de acordo com a dificuldade escolhida.')
+
+partidas = 0
+acertos = 0
 
 jogar_novamente = 's'
 
 while jogar_novamente.lower() == 's':
-    numero_secreto = random.randint(1, 100)
-    tentativas = 0
+    partidas += 1
 
+    print("\nEscolha a dificuldade:")
+    print("1 - Fácil (1 a 50)")
+    print("2 - Médio (1 a 100)")
+    print("3 - Difícil (1 a 200)")
+    
     while True:
+        dificuldade = input("Digite o número da dificuldade desejada: ")
+        if dificuldade in ['1', '2', '3']:
+            break
+        else:
+            print("Opção inválida. Escolha 1, 2 ou 3.")
+
+    if dificuldade == '1':
+        limite = 50
+        max_tentativas = 10
+    elif dificuldade == '2':
+        limite = 100
+        max_tentativas = 10
+    else:
+        limite = 200
+        max_tentativas = 15
+
+    numero_secreto = random.randint(1, limite)
+    tentativas = 0
+    palpites_feitos = []
+
+    print(f"\n🔢 Adivinhe um número de 1 a {limite}. Você tem {max_tentativas} tentativas.")
+
+    frases_erro = [
+        "Hmm... ainda não!", 
+        "Tente outra vez!", 
+        "Quase lá!", 
+        "Não foi dessa vez!", 
+        "Você está chegando perto!"
+    ]
+
+    while tentativas < max_tentativas:
         palpite = input("Digite seu palpite: ")
 
         if not palpite.isdigit():
-            print("Por favor, digite apenas números!")
+            print("❌ Por favor, digite apenas números!")
             continue
 
         palpite = int(palpite)
+        palpites_feitos.append(palpite)
         tentativas += 1
 
         if palpite < numero_secreto:
-            print("É maior!!")
+            print("📈 É maior!!", random.choice(frases_erro))
         elif palpite > numero_secreto:
-            print("É menor!!")
+            print("📉 É menor!!", random.choice(frases_erro))
         else:
-            print(f"Parabéns! Você descobriu o número {numero_secreto} em {tentativas} tentativas!")
+            print(f"🎉 Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas!")
+            acertos += 1
             break
+        print(f"📋 Palpites até agora: {palpites_feitos}")
 
-    jogar_novamente = input("Deseja jogar novamente? (s/n): ")
-
-    if jogar_novamente.lower() == 's':
-        print ("Vamos jogar de novo!!")
     else:
-        print ("Obrigada por jogar!!")
-        input("Aperte Enter para sair do jogo.")
-      
+        print(f"\n😢 Você usou todas as {max_tentativas} tentativas.")
+        print(f"O número secreto era: {numero_secreto}")
+
+    print(f"\n🧮 Estatísticas: {partidas} partida(s), {acertos} acerto(s).")
+
+    jogar_novamente = input("\nDeseja jogar novamente? (s/n): ")
+
+print("\n🎊 Obrigada por jogar! Até a próxima!")
+input("Aperte Enter para sair do jogo.")
+
